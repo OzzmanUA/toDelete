eea977ca648af4de198e78e1fc3d9378441bb18b
OzzmanUA <Ozzman.zp@gmail.com>
2022-09-21 17:36:27 +0300

    First commit

---
+++ File.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class File
+    {
+        public string Name { get; set; }
+        public string Extantion { get; set; }
+        public long Size { get; set; }
+        public DateTime Creation { get; set; }
+        public File()
+        {
+            Name = "Example";
+            Extantion = "txt";
+            Size = 1;
+            Creation = DateTime.Now;
+        }
+        public File(string name, string extantion, long size, DateTime creation)
+        {
+            Name = name;
+            Extantion = extantion;
+            Size = size;
+            Creation = creation;
+        }
+        public string FileSize()
+        {
+            if (Size < 1024) return Size.ToString() + "byte";
+            else
+            {
+                if ((Size / 1024) < 1024) return (Size / 1024).ToString() + "." + ReturnTwoNumbers(Size % 1024).ToString() + "kb";
+                else
+                {
+                    if ((Size / 1048576) < 1024) return (Size / 1048576).ToString() + "." + ReturnTwoNumbers(Size % 1048576).ToString() + "mb";
+                    else return (Size / 1073741824).ToString() + "." + ReturnTwoNumbers(Size % 1073741824).ToString() + "gb";
+
+                }
+            }
+        }
+        public void Print()
+        {
+            Console.Write($"File: {Name}.{Extantion}    Created: {Creation.Day}.{Creation.Month}.{Creation.Year}     Size: ");
+            Console.WriteLine(FileSize());
+        }
+        long ReturnTwoNumbers(long number)
+        {
+            int[] arr = new int[9] { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            if (number < 100)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == number) number /= 10;
+                }
+                return number;
+            }
+            else
+            {
+                while (number > 100) number /= 10;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == number) number /= 10;
+                }
+                return number;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Data;
+using File = Data.File;
+
+File exemple = new File()
+{
+    Name = "Note",
+    Extantion = "txt",
+    Size = 1233,
+    Creation = DateTime.Parse("05.09.2010")
+};
+
+Console.WriteLine(exemple.FileSize());
+
+File[] files = new File[]
+{
+    new File("List","txt",234,DateTime.Parse("12.03.2007")),
+    new File("Photo","jpeg",2342334,DateTime.Parse("30.12.2020")),
+    new File("Listuaua","txt",23445554,DateTime.Parse("23.03.2021")),
+    new File("Start","exe",236704,DateTime.Parse("23.09.2022")),
+    new File("boot","ini",4334,DateTime.Parse("23.09.2022")),
+    new File("FullListua","txt",9845623224,DateTime.Parse("12.03.2007")),
+    new File("Test","exe",1234,DateTime.Parse("12.03.2007"))
+};
+for (int i = 0; i < files.Length; i++)
+{
+    files[i].Print();
+}
+
+Console.WriteLine("*************************************************");
+
+for (int i = 0; i < files.Length; i++)
+{
+    if (files[i].Extantion == "txt") files[i].Print();
+}
+
+Console.WriteLine("*************************************************");
+
+for (int i = 0; i < files.Length; i++)
+{
+    for (int j = 0; j < files[i].Name.Length; j++)
+    {
+        if (files[i].Name[j] == 'u' && files[i].Name[j + 1] == 'a')
+        {
+            files[i].Print();
+            break;
+        }
+    }
+}
+
+Console.WriteLine("*************************************************");
+
+for(int i = 0; i < files.Length; i++)
+{
+    if(files[i].Creation < DateTime.Parse("17.09.2022")) files[i].Print();
+}
